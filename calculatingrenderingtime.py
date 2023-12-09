c9ecb195f8b43209617008e976ea4fea478ad12c
Junseo <j.qus2046@gmail.com>
2023-12-09 13:30:21 +0900

    #3 conC:/Users/user/AppData/Local/Programs/Python/Python312/python.exe "c:/Users/user/OneDrive/바탕 화면/BJScoding/schoolshit/collaborative project/calculatingrenderingtime.py"confirmed

--- calculatingrenderingtime.py
+++ calculatingrenderingtime.py
@@ -1,15 +1,15 @@
-#Calculating time it takes to render with Evee and Cycle
 import math
 
-# Step 1: Get user input for camera coordinates
+# Step 1: Ask for the x and y values for the camera location
 camera_x = float(input("Enter the x-coordinate of the camera: "))
 camera_y = float(input("Enter the y-coordinate of the camera: "))
 
-# Object is at the center (0, 0)
-object_x, object_y = 0, 0
+# Step 2: Calculate the distance using the Pythagorean theorem
+object_x = 0  # Assuming the object is at the center (0, 0)
+object_y = 0
 
-# Step 2: Calculate distance using Pythagorean theorem
 distance = math.sqrt((camera_x - object_x)**2 + (camera_y - object_y)**2)
 
-# Step 3: Print out the distance
-print(f"The distance between the camera and the object is: {distance}")
+# Step 3: Print out the distance and how it was calculated
+print(f"The distance between the camera and the object is {distance:.2f} meters.")
+print(f"Calculation: sqrt(({camera_x} - {object_x})^2 + ({camera_y} - {object_y})^2)")
